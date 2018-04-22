410d23d4db4b78e7ef6ca98d80f96555e9106bab
alexfromsocal <alittaua67@gmail.com>
2018-04-21 19:53:27 -0700

    AdminController created, minor changes to SMS_handler.

--- SMS_handler.py
+++ SMS_handler.py
@@ -10,5 +10,14 @@ app = Flask(__name__)
 @app.route("/sms", methods=['GET', 'POST'])
 
 
+
+#Receives the incoming SMS to process further for our client.
 def incoming_sms():
     
+    #Gets the message from sender and grabs the body of it.
+    body = request.values.get('Body', None)
+    resp = MessagingResponse()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
