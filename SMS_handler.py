d5d2f68faf9990109ad23fe6f4649fd634e77c31
alexfromsocal <alittaua67@gmail.com>
2018-04-22 00:42:53 -0700

    I think I figured out the Twilio service, again...

--- SMS_handler.py
+++ SMS_handler.py
@@ -29,7 +29,7 @@ def incoming_sms():
    
     #Gets the message from sender and grabs the body of it.
     body = request.values.get('Body', None)
-    resp = MR.MessagingResponse()                               #Taken from Twilio helper library
+    resp = MessagingResponse()                               #Taken from Twilio helper library
     resp.message("This is the ship that made the Kessel Run in fourteen parsecs?")
 
     return str(resp)
