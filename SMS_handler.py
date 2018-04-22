137bfa658841bacd01edfc56b59e267946b2f396
alexfromsocal <alittaua67@gmail.com>
2018-04-22 10:22:02 -0700

    Attempted to merge all code into one master handler file. I don't think it worked too well.

--- SMS_handler.py
+++ SMS_handler.py
@@ -1,10 +1,15 @@
 #from flask import Flask, request, redirect
 #from twilio.twiml.messaging_response import MessagingResponse as MR
 from twilio.rest import Client
+from flask import url_for, session, Flask, request
 from configparser import ConfigParser
-from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from AdminPrivilegeCheck import checkNum
+import os, sys
+import random
+import numpy
+from twilio.rest import Client
+from configparser import ConfigParser
 #import importlib
 
 
@@ -12,35 +17,95 @@ from AdminPrivilegeCheck import checkNum
 
 app = Flask(__name__)
 
-config = ConfigParser()
-config.read('config.cfg')
-account = config.get('auth', 'account')
-token = config.get('auth', 'token')
 
-client = Client(account, token)
-
-
-@app.route("/sms_inbound", methods=['GET', 'POST'])
+@app.route('/sms_inbound')
 
 
 #Receives the incoming SMS to process further for our client.
 def incoming_sms():
-   
-    #Gets the message from sender and grabs the body of it.
+    configTwilio = ConfigParser()
+    configTwilio.read('config.cfg')
+    account = configTwilio.get('auth', 'account')
+    token = configTwilio.get('auth', 'token')
+    servicesid = configTwilio.get('auth', 'servicesid')
+    logoncode = configTwilio.get('key', 'passcode')
+    client = Client(account, token)
+
+
+    phone_numbers = client.messaging \
+                      .services(sid=servicesid) \
+                      .phone_numbers \
+                      .list()
+
     body = request.values.get('Body', None)
-    resp = MessagingResponse()                               #Taken from Twilio helper library
-    resp.message("This is the ship that made the Kessel Run in fourteen parsecs?")
-
-
+    response =MessagingResponse()
+    
+   
     if str(body).lower() == "logon":
-        resp.message("Checking your credentials...")
-        print(resp)
-        checkNum(request.values.get('From'))
+        isAdmin = checkAdmin(request.values.get('From'), client, phone_numbers)
+        print("logged")
 
     else:
-        None
+        response.message("Your features aren't valid yet")
+        
+    #Gets the message from sender and grabs the body of it.
+    
+                             #Taken from Twilio helper library
+    return str(response)
 
-    return str(resp)
+def redirect_to_first_question(response, survey):
+    first_question = survey.questions.order_by('id').first()
+    first_question_url = url_for('question', question_id=first_question.id)
+    response.redirect(url=first_question_url, method='GET')
+
+
+def welcome_user(survey, send_function):
+    welcome_text = 'Welcome to the %s' % survey.title
+    send_function(welcome_text)
+
+
+def survey_error(survey, send_function):
+    if not survey:
+        send_function('Sorry, but there are no surveys to be answered.')
+        return True
+    elif not survey.has_questions:
+        send_function('Sorry, there are no questions for this survey.')
+        return True
+    return False
+
+
+def checkAdmin(PhNum, client, phone_numbers):
+  
+    list = readToList()
+   
+
+    #serverPhone = phone_numbers[0].phone_number
+    strOfNumbers = ''.join(str(n) for n in list)
+    print(strOfNumbers)
+    if PhNum in list:
+        message = client.messages.create(
+        to=PhNum, 
+        from_=phone_numbers[0].phone_number,
+        body="What is your passkey?")
+
+        print(message.sid)
+        return True
+
+        
+    else:
+        message = client.messages.create(
+        to=PhNum, 
+        from_=phone_numbers[0].phone_number,
+        body="You cannot access this.")
+        print(message.sid)
+        return False
+
+def readToList():
+    file = open("AdminList.txt", "r")
+    list = file.read().splitlines()
+    file.close()
+    return list
+
 
 
 @app.route("/sms_outbound", methods=['GET', 'POST']) 
