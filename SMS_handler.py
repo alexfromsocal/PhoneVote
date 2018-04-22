2dff6cbaa3d6de72619cbae11b2ddde0aae2031b
alexfromsocal <alittaua67@gmail.com>
2018-04-21 20:54:23 -0700

    Updated SMS_handler.py for testing

--- SMS_handler.py
+++ SMS_handler.py
@@ -1,14 +1,22 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse as MR
 from twilio.rest import Client
+from configparser import ConfigParser as cp
 import importlib
+
 
 
 
 app = Flask(__name__)
 
-@app.route("/sms", methods=['GET', 'POST'])
+config = cp.ConfigParser()
+config.read('config.cfg')
+account = config.get('auth', 'account')
+token = config.get('auth', 'token')
 
+
+@app.route("/sms", methods=['GET', 'POST'])
+client = Client(account, token)
 
 
 #Receives the incoming SMS to process further for our client.
@@ -16,7 +24,10 @@ def incoming_sms():
     
     #Gets the message from sender and grabs the body of it.
     body = request.values.get('Body', None)
-    resp = MessagingResponse()
+    resp = MR.MessagingResponse()                               #Taken from Twilio helper library
+    resp.message("This is the ship that made the Kessel Run in fourteen parsecs?")
+
+    return str(resp)
 
 
 if __name__ == "__main__":
